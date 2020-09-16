6d930ca8fdba54105cfda3d58665f5593b17c173
Funke Adebisi <mak_funke@yahoo.com>
2020-09-15 21:18:59 -0400

    Update about.php

--- about.php
+++ about.php
@@ -1,19 +1,19 @@
 <?php
 include ('include/header.html');
 ?>
-   <div class="row text-center greenyellow">
-	 <h2>ABOUT FTMASOFT</h2>
- <hr>
+<div class="row text-center greenyellow">
+    <h2>ABOUT US</h2>
+    <hr>
 </div>
- <div class="container">
-      <div class="row pad">
+<div class="container">
+    <div class="row pad">
         <div class="col-lg-12 ">
-<p><span class="indent">We</span> have passion for application development with the objective of providing customized software, mobile applications, website design and development services for individuals, small and medium-sized businesses, non-profit organization and multinational organization.</p>
-<p><span class="indent">We</span> love to give your business a professional presence through webite design and development services and IT Support Services. We will create the entire software and web packages including banners, logos, customized graphics that best represent your business. We will build professional, attractive and mobile friendly software, web applications and websites that will boost sales and awareness for your business. </p>
-<p><span class="indent">Writing</span> software applications that bring solution to real life problem is our goal. We can put a smile on your face, if you need a software application that will make life easy for you or your organization, just call or email us and let us discuss about it.</p>
- </div>
+            <p><span class="indent">Lorem ipsum</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p><span class="indent">Lorem ipsum</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <p><span class="indent">Lorem ipsum</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </div>
-        </div>
+    </div>
+</div>
 <?php
 include ('include/footer.html');
 ?>
